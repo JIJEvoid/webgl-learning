5a0e66b56f68fb42078aeb9cc5ad3b97729302e1
JIJEvoid <jijevoid@gmail.com>
2021-08-25 15:01:47 +0800

    add

--- demo1/js/12_texture.js
+++ demo1/js/12_texture.js
@@ -33,10 +33,10 @@ function main() {
 function initVertexBuffer(gl) {
     //指定图片位置
     var verticesTexCoords = new Float32Array([
-        -0.5,0.5,0.0,1.0,
-        -0.5,-0.5,0.0,0.0,
-        0.5,0.5,1.0,1.0,
-        0.5,-0.5,1.0,0.0
+        -1.0,1.,0.0,1.0,
+        -1.,-1.,0.0,0.0,
+        1.,1.,1.0,1.0,
+        1.,-1.,1.0,0.0
     ]);
     var n = 4;
     var vertexTexCoordBuffer = gl.createBuffer();
